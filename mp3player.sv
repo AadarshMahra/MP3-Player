4b50de02ac61000803a6c852a6c7c34968a7a6d9
Aadarsh <amahra2@illinois.edu>
2022-11-17 21:20:23 -0600

    not compiling pt.2

--- mp3player.sv
+++ mp3player.sv
@@ -9,6 +9,8 @@
   --      UIUC ECE Department                                              --
   ---------------------------------------------------------------------------*/
 // Top-level module that integrates the Nios II system with the rest of the hardware
+	
+
 
 module mp3player(  	 	  input	        MAX10_CLK1_50, 
 					  input  [1:0]  KEY,
@@ -24,14 +26,35 @@ module mp3player(  	 	  input	        MAX10_CLK1_50,
 					  output 		DRAM_UDQM,
 					  output	    DRAM_RAS_N,
 					  output	    DRAM_WE_N,
-					  output	    DRAM_CLK
+					  output	    DRAM_CLK,
+					  inout [15:0] ARDUINO_IO
 				  
 				  );
 				  
+				   assign ARDUINO_IO[3] = aud_mclk_ctr[1]; 
+
+					always_ff @(posedge MAX10_CLK2_50) begin
+						aud_mclk_ctr <= aud_mclk_ctr + 1;
+					end
+					
+		
+					assign SCL_IN = SCL; 
+					assign SCL = SCL_OE ? 1'b0 : 1'bz; 
+					
+					assign SDA_IN = SDA; 
+					assign SDA = SDA_OE ? 1'b0 : 1'bz;
+					
+					assign ARDUINO_IO[15] = SCL;
+					assign ARDUINO_IO[14] = SDA;
 				  // You need to make sure that the port names here are identical to the port names at 
 				  // the interface in lab61_soc.v
 				  mp3player_soc soc (.clk_clk(MAX10_CLK1_50),
 											 .reset_reset_n(KEY[0]), 
+											 // I2C
+											 i2c0_sda_in(SDA_IN); 
+											 i2c0_scl_in(SCL_IN); 
+											 i2c0_sda_oe(SDA_OE); 
+											 i2c0_scl_oe(SCL_OE); 
 											//SDRAM
 											.sdram_clk_clk(DRAM_CLK),                            //clk_sdram.clk
 											.sdram_wire_addr(DRAM_ADDR),                         //sdram_wire.addr
@@ -44,6 +67,7 @@ module mp3player(  	 	  input	        MAX10_CLK1_50,
 											.sdram_wire_ras_n(DRAM_RAS_N),                       //.ras_n
 											.sdram_wire_we_n(DRAM_WE_N)                          //.we_n
 											 );
+	
 											 
 				//Instantiate additional FPGA fabric modules as needed		  
 endmodule
